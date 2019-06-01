b47a7dbe224212f84ae51ac8faf215eea701617c
Tay <tayd93@hotmail.com>
2019-06-01 14:45:53 -0600

    Refactos attributes from nil to empty arrays

--- lib/key.rb
+++ lib/key.rb
@@ -2,12 +2,14 @@ class Key
   attr_reader :a, :b, :c, :d
 
   def initialize(key = nil) #optional, maybe make a conditional that will get key, if nil.
-    @a = nil
-    @b = nil
-    @c = nil
-    @d = nil
-    @random = nil
+    @a = []
+    @b = []
+    @c = []
+    @d = []
+    @random_number = nil
+    @split_digits = nil
     generate
+    digits
   end
 
   def generate
@@ -15,11 +17,11 @@ class Key
   end
 
   def digits #now I need to set digits equal to abcd
-    @random.chars
+    @split = @random.chars
   end
 
-  def set_digits
-    
+  def assign_digits
+
 
   end
 
--- test/key_test.rb
+++ test/key_test.rb
@@ -12,18 +12,20 @@ class KeyTest < MiniTest::Test
   end
 
   def test_key_attributes #is this enough?
-    assert_nil @key.a
-    assert_nil @key.b
-    assert_nil @key.c
-    assert_nil @key.d
+    assert_equal [], @key.a
+    assert_equal [], @key.b
+    assert_equal [], @key.c
+    assert_equal [], @key.d
   end
 
   def test_generate
     assert_instance_of Integer, @key.generate.to_i
   end
 
-  def test_set_digits
-    @key.set_digits 
+  def test_assign_digits
+    skip
+#binding.pry
+    @key.set_digits
     assert_instance_of Integer, @key.a
     assert_instance_of Integer, @key.b
     assert_instance_of Integer, @key.c
