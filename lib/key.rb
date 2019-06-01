295d41f3a4de297c51b2c7416bd1199617785917
Tay <tayd93@hotmail.com>
2019-06-01 10:39:48 -0600

    Creates group test

--- lib/key.rb
+++ lib/key.rb
@@ -1,8 +1,11 @@
 class Key
-  attr_reader
+  attr_reader :a, :b, :c, :d 
 
   def initialize(key = nil) #optional, maybe make a conditional that will ket key, if nil.
-
+    @a = rand(9)
+    @b = rand(9)
+    @c = rand(9)
+    @d = rand(9)
   end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/key'
+require 'pry'
 
 class KeyTest < MiniTest::Test
   def setup
@@ -8,5 +9,16 @@ class KeyTest < MiniTest::Test
 
   def test_key_exists
     assert_instance_of Key, @key
+  end
+
+  def test_key_attributes #is this enough?
+    assert_instance_of Integer, @key.a
+    assert_instance_of Integer, @key.b
+    assert_instance_of Integer, @key.c
+    assert_instance_of Integer, @key.d
+  end
+
+  def test_group
+    assert_equal 5, key.group.length
   end
 end
