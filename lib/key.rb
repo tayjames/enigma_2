3094724112d74f08e0e3a1c6db34d743a129371f
Tay <tayd93@hotmail.com>
2019-06-01 10:20:25 -0600

    Passes existence test

---
+++ lib/key.rb
@@ -0,0 +1,8 @@
+class Key
+  attr_reader
+
+  def initialize(key = nil) #optional, maybe make a conditional that will ket key, if nil.
+
+  end
+
+end
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,11 +1,12 @@
 require './test/test_helper'
+require './lib/key'
 
 class KeyTest < MiniTest::Test
   def setup
-
+    @key = Key.new
   end
 
   def test_key_exists
-    assert_instance_of Key, @key 
+    assert_instance_of Key, @key
   end
 end
