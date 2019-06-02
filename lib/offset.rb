f0904ec65671fbe2a44586947172d9f1b72aa7a6
Tay <tayd93@hotmail.com>
2019-06-01 20:04:47 -0600

    Creates rough draft for assign test/method

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,16 +1,23 @@
 require 'pry'
 
 class Offset
-  attr_reader
+  attr_reader :a, :b, :c, :d
 
   def initialize
-
   end
 
   def get_date_info
     date = Date.today.strftime("%d%m%y").to_i
     square = (date ** 2).to_s
-    square[-4..-1]
+    last_four = square[-4..-1]
+  end
+
+  def assign (last_four)
+    @a = last_four[0]
+    @b = last_four[1]
+    @c = last_four[2]
+    @d = last_four[3]
+
   end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -15,7 +15,22 @@ class OffsetTest < MiniTest::Test
 
   def test_get_date_info
     #binding.pry
+    #expected = Date.today.strftime("%d%m%y")
     assert_equal "3161", @offset.get_date_info
+  end
+
+  def test_assign
+    #how can i incorparate this into mocks/stubs
+    #date = 010119
+    #square = 10239461
+    #last 4 = 9461
+    last_four = 3161
+    binding.pry
+    @offset.assign(last_four)
+    assert_equal 3, @offset.a
+    assert_equal 1, @offset.b
+    assert_equal 6, @offset.c
+    assert_equal 1, @offset.d
   end
 
 end
