4812108213c3543f0b092233c451ff5bfec243ac
Tay <tayd93@hotmail.com>
2019-06-03 21:24:41 -0600

    Refactors Offset test/class to be more descriptive

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Offset
-  attr_reader :a, :b, :c, :d, :date
+  attr_reader :a_offset, :b_offset, :c_offset, :d_offset, :date
 
   def initialize(date = nil)
     set_date(date)
@@ -19,10 +19,10 @@ class Offset
   def square_date
     square = (@date ** 2).to_s
     last_four = square[-4..-1]
-    @a = last_four[0].to_i
-    @b = last_four[1].to_i
-    @c = last_four[2].to_i
-    @d = last_four[3].to_i
+    @a_offset = last_four[0].to_i
+    @b_offset = last_four[1].to_i
+    @c_offset = last_four[2].to_i
+    @d_offset = last_four[3].to_i
     last_four
   end
 
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -7,7 +7,7 @@ class OffsetTest < MiniTest::Test
 
   def setup
     @offset = Offset.new
-    @setoff = Offset.new(10119) #change this to ddmmyy
+    @setoff = Offset.new(10119)
   end
 
   def test_offset_exists
@@ -16,34 +16,22 @@ class OffsetTest < MiniTest::Test
   end
 
   def test_set_date
-    assert_equal 20619, @offset.date #6 digits
+    assert_equal 30619, @offset.date #6 digits
     assert_equal 10119, @setoff.date
   end
 
   def test_square_date
-    #expected = Date.today.strftime("%d%m%y")
-    #binding.pry
-    assert_equal "4161", @setoff.square_date
     assert_equal "3161", @offset.square_date
-    #assert_equal ""
-  #def test_assign
-    #how can i incorparate this into mocks/stubs
-    #date = 010119
-    #square = 10239461
-    #last 4 = 9461
-    #last_four = 3161
-    #@offset.assign(last_four)
-    #@offset.square_date
-    assert_equal 3, @offset.a
-    assert_equal 1, @offset.b
-    assert_equal 6, @offset.c
-    assert_equal 1, @offset.d
+    assert_equal "4161", @setoff.square_date
 
-    assert_equal 4, @setoff.a
-    assert_equal 1, @setoff.b
-    assert_equal 6, @setoff.c
-    assert_equal 1, @setoff.d
-    #binding.pry
+    assert_equal 3, @offset.a_offset
+    assert_equal 1, @offset.b_offset
+    assert_equal 6, @offset.c_offset
+    assert_equal 1, @offset.d_offset
+
+    assert_equal 4, @setoff.a_offset
+    assert_equal 1, @setoff.b_offset
+    assert_equal 6, @setoff.c_offset
+    assert_equal 1, @setoff.d_offset
   end
-
 end
